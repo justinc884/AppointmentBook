eddc9df94be1c757af2258a4f22fb7041f347de4
JustinChan <justinc884@nycstudents.net>
2025-09-26 15:32:30 -0400

    I am working on an appointment book.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for(int i = 10; i < 15; i++) schedule[1][i] = true;
+        for(int i = 30; i < 45; i++) schedule[1][i] = true;
+        for(int i = 50; i < 60; i++) schedule[1][i] = true;
+        a.printPeriod(2);
+        a.findFreeBlock(2, 15);
+    }
+}
